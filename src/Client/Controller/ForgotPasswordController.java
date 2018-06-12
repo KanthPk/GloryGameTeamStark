687653e6fd2167d3eaa7cc5a4ef90950fa77edc9
Subash Perera <39507540+Suba95@users.noreply.github.com>
2018-06-12 23:16:08 +0530

    Task25
    Email Verification to login

--- src/Client/Controller/ForgotPasswordController.java
+++ src/Client/Controller/ForgotPasswordController.java
@@ -5,15 +5,23 @@
  */
 package Client.Controller;
 
+import glory_services.MessageService;
+import glory_services.SendEmailService;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -41,10 +49,16 @@ public class ForgotPasswordController implements Initializable {
 
     @FXML
     private Button btnBack;
+    @FXML
+    private Button btnSendCode;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        btnSendCode.setVisible(true);
+        //txtNewPassword.setVisible(false);
+       // txtConfirmpassword.setVisible(false);
+        
     }
 
     @FXML
@@ -62,5 +76,41 @@ public class ForgotPasswordController implements Initializable {
             //save the data
         } catch (Exception e) {
         }
+    }
+    @FXML
+    void btnSendcodeClicked(ActionEvent event) {
+          try {                        
+                //validatorService.getValidaterMessage("MAIL CONFIRMATION", "Please enter your confirmation code to verify your email", true, true, false);//AshansCode
+                String usarMail = "maduperera106@gmail.com";
+                //Set the current genarated code
+                //setUserRecievedCode(id); 
+               //Genarate randome number and send email
+                    Random random = new Random();
+                    String id = String.format("%04d", random.nextInt(10000));
+                    SendEmailService sc = new SendEmailService();            
+                    sc.sendVerificationCode(id,usarMail);
+           
+                    
+                       AnchorPane layout;
+                       Stage stage;
+                       String header = "MAIL CONFIRMATION";
+                        String body = "Please enter your confirmation code to verify your email";
+                       MessageService.setMakeMessageUI("mail", header, body);
+                       // layout = FXMLLoader.load(getClass().getResource("/UI/CommenMessage.fxml"));
+                        //CommenMessageController cmc = new CommenMessageController();  
+                        
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/UI/CommenMessage.fxml"));
+                        Parent parentHome = (Parent) fxmlLoader.load();
+                        CommenMessageController controller = fxmlLoader.<CommenMessageController>getController();
+                        controller.setGenaratedCode(id);
+                        controller.setRecievermail(usarMail);
+                     
+                        stage = new Stage();
+                       stage.setScene(new Scene(parentHome));
+                       stage.setResizable(false);
+                      stage.initStyle(StageStyle.UNDECORATED);
+                        stage.show();
+                    } catch (Exception e) {
+                    }
     }
 }
