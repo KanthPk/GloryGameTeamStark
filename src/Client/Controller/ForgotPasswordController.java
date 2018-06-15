317f1e1cd0fdc4656c8ee7d0f4cc72ee7b463133
KanthPk <39432617+KanthPk@users.noreply.github.com>
2018-06-15 21:23:20 +0530

    Revert "Task25"
    This reverts commit 687653e6fd2167d3eaa7cc5a4ef90950fa77edc9.

--- src/Client/Controller/ForgotPasswordController.java
+++ src/Client/Controller/ForgotPasswordController.java
@@ -5,25 +5,21 @@
  */
 package Client.Controller;
 
+
 import glory_schema.ConstantElement;
 import glory_services.MessageService;
 import glory_services.SendEmailService;
 import glory_services.ValidatorService;
+
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -53,6 +49,7 @@ public class ForgotPasswordController implements Initializable {
 
     @FXML
     private Button btnBack;
+
     @FXML
  
     private ValidatorService validatorService;
@@ -89,6 +86,12 @@ public class ForgotPasswordController implements Initializable {
         });
 
         
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+
     }
 
     @FXML
@@ -106,41 +109,5 @@ public class ForgotPasswordController implements Initializable {
             //save the data
         } catch (Exception e) {
         }
-    }
-    @FXML
-    void btnSendcodeClicked(ActionEvent event) {
-          try {                        
-                //validatorService.getValidaterMessage("MAIL CONFIRMATION", "Please enter your confirmation code to verify your email", true, true, false);//AshansCode
-                String usarMail = "maduperera106@gmail.com";
-                //Set the current genarated code
-                //setUserRecievedCode(id); 
-               //Genarate randome number and send email
-                    Random random = new Random();
-                    String id = String.format("%04d", random.nextInt(10000));
-                    SendEmailService sc = new SendEmailService();            
-                    sc.sendVerificationCode(id,usarMail);
-           
-                    
-                       AnchorPane layout;
-                       Stage stage;
-                       String header = "MAIL CONFIRMATION";
-                        String body = "Please enter your confirmation code to verify your email";
-                       MessageService.setMakeMessageUI("mail", header, body);
-                       // layout = FXMLLoader.load(getClass().getResource("/UI/CommenMessage.fxml"));
-                        //CommenMessageController cmc = new CommenMessageController();  
-                        
-                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/UI/CommenMessage.fxml"));
-                        Parent parentHome = (Parent) fxmlLoader.load();
-                        CommenMessageController controller = fxmlLoader.<CommenMessageController>getController();
-                        controller.setGenaratedCode(id);
-                        controller.setRecievermail(usarMail);
-                     
-                        stage = new Stage();
-                       stage.setScene(new Scene(parentHome));
-                       stage.setResizable(false);
-                      stage.initStyle(StageStyle.UNDECORATED);
-                        stage.show();
-                    } catch (Exception e) {
-                    }
     }
 }
